7f2ee741dfc97a8c8083eb0e9f9d4f4947813732
Igorbrito77 <britoigor77@gmail.com>
2019-05-09 01:43:38 -0300

    teste properties swagger

--- routes/index.js
+++ routes/index.js
@@ -3,39 +3,6 @@ var router = express.Router();
 var db = require('../database/queries');
 //var doc = require('../documentacao');
 
-/**
- * @swagger
-* definitions:
-*  Dados:      
-*              properties:
-*                 aluno:
-*                    schema:
-*                      type: object 
-*                      $ref: '#/definitions/Aluno'
-*                 endereco:
-*                    schema:
-*                      type: object 
-*                      $ref: '#/definitions/Dados'
-*
-*  Aluno: 
-*              properties: 
-*                  nome:
-*                      type: string
-*                  matricula: 
-*                      type: string
-*                  nota: 
-*                      type: number
-*                      format: float 
-*  
-*  Endereco:
-*              properties: 
-*                   rua:
-*                       type: string
-*                   numero: 
-*                       type: integer
-*                   bairro: 
-*                       type: string 
-*/
 
  /**
  * @swagger
@@ -100,7 +67,6 @@ var db = require('../database/queries');
 *           properties: 
 *              Dados:
 *                type: object 
-*                $ref: '#/definitions/Dados'
 *           example:      
 *              {aluno : { nome : Ana, matricula : "12345", nota : 9.7}, endereco: {rua : Rua Brasil, numero : 456, bairro : Campo Grande}}
 *     responses:
